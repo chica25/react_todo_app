d006f16c49b7f4d73aad4eec4bd0b19120717b49
chica25 <pfco520@gmail.com>
2021-07-02 23:21:46 -0400

    added buttons to the form

--- src/App.js
+++ src/App.js
@@ -10,9 +10,12 @@ function App(props) {
             Daily Task
           </label>
         </h2>
-        <input type="text" id="new-todo-input" className="input-lg" name="text" autoComplete="off" />
-        <button type="submit" className="btn bnt-primary btn-lg"Add></button>
-      </form>
+          <input type="text" id="new-todo-input" className="input-lg" name="text" autoComplete="off" />
+          <button type="submit" className="btn bnt-primary btn-lg"Add></button>
+        </form>
+        <div className="filters btn-group stack-exception">
+          <button type="button" className="btn toggle-btn" aria-pressed="false"></button>
+        </div>
     </div>
   );
 }
