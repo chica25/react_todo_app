014488f0481993906fb13ed455597592bc6d1eea
chica25 <pfco520@gmail.com>
2021-07-02 23:18:43 -0400

    Implement form in app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 
 function App(props) {
-console.log(props)
-  // const subject = props.subject;
   return (
-    <div></div>
+    <div className="todo-app stack-lg">
+      <h1>Todo App</h1>
+      <form>
+        <h2 className="label-wrapper">
+          <label htmlFor="new-todo-input" className="label-lg">
+            Daily Task
+          </label>
+        </h2>
+        <input type="text" id="new-todo-input" className="input-lg" name="text" autoComplete="off" />
+        <button type="submit" className="btn bnt-primary btn-lg"Add></button>
+      </form>
+    </div>
   );
 }
 
